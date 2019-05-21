fc9e9fbf22e3404ca2996abf19a0e414d6296d04
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2019-05-21 22:55:43 +0900

    Mod webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,19 +7,19 @@ const HtmlPlugin = require("html-webpack-plugin");
 const WorkBoxPlugin = require("workbox-webpack-plugin");
 
 module.exports = merge(common, {
-    mode: "production",
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env.NODE_ENV": JSON.stringify("production")
-        }),
-        new DotEnv(),
-        new WorkBoxPlugin.InjectManifest({
-            swSrc: path.join(__dirname, "src/sw.js"),
-            swDest: "sw.js"
-        }),
-        new HtmlPlugin({
-            hash: true,
-            template: path.join(__dirname, "src/index.html.ejs")
-        })
-    ]
+  mode: "production",
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production")
+    }),
+    new DotEnv(),
+    new WorkBoxPlugin.InjectManifest({
+      swSrc: path.join(__dirname, "src/sw.js"),
+      swDest: "sw.js"
+    }),
+    new HtmlPlugin({
+      hash: true,
+      template: path.join(__dirname, "src/index.html.ejs")
+    })
+  ]
 });
